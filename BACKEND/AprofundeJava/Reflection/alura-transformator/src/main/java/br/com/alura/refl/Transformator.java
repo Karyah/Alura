9e9c5384dc02bd45072efac7f2aa9924b0a75a8e
Karyah <mastrogiacomokaren@gmail.com>
2024-10-28 10:20:52 -0300

    Mais contéudo das aulas de Reflection

--- BACKEND/AprofundeJava/Reflection/alura-transformator/src/main/java/br/com/alura/refl/Transformator.java
+++ BACKEND/AprofundeJava/Reflection/alura-transformator/src/main/java/br/com/alura/refl/Transformator.java
@@ -1,18 +1,44 @@
 package br.com.alura.refl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Transformator {
 	
 	public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, SecurityException{
 		/* i - input; o - output*/
 		Class<?> source = input.getClass();
-		Class<?> target = source.forName(source.getClass() + "DTO");
+		Class<?> target = Class.forName(source + "DTO");
 		
 		O targetClass = (O) target.getDeclaredConstructor().newInstance();
 		
+		Field[] sourceFields =source.getDeclaredFields();
+		Field[] targetFields = target.getDeclaredFields();
+		
+		Arrays.stream(sourceFields).forEach(sourceField -> 
+				Arrays.stream(targetFields).forEach(targetField -> {
+					validate(sourceField, targetField);	
+					
+					try {
+						targetField.set(targetClass, sourceField.get(input));
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}));
+		
 		return targetClass;
 	}
+	
+	public void validate(Field sourceField, Field targetField) {
+		if(sourceField.getName().equals(targetField.getName()) && sourceField.getType().equals(targetField.getType())) {
+			sourceField.setAccessible(true);
+			targetField.setAccessible(true); //permite que campos privados possam ser acessados. quebra o encapsulamento.
+			
+		}
+	}
 }
-
-/*Outra API: Generics, podemos receber qualquer coisa, sem necessidade de especificar tipo*/
